628e50158807363dab3a1cc2fb61ff63b1ec4efb
dung997 <118365107+dung997@users.noreply.github.com>
2024-03-07 05:35:20 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ function firstQuestion(){
         imageUrl: 'img/KakaoTalk_20240307_000806300_02.jpg',
         imageWidth: 300,
         imageHeight: 300,
-        background: '#fff url("img/iput-KakaoTalk_20240307_000806300_01.jpg")',
+        background: '#fff url("img/iput-KakaoTalk_20240307_000806300.jpg")',
         imageAlt: 'Custom image',
         confirmButtonText: CONFIG.btnIntro
       }).then(function(){
@@ -104,7 +104,7 @@ $('#yes').click(function() {
         width: 900,
         padding: '3em',
         html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
-        background: '#fff url("img/iput-KakaoTalk_20240307_000806300_01.jpg")',
+        background: '#fff url("img/iput-KakaoTalk_20240307_000806300.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
               url("img/giphy2.gif")
@@ -119,7 +119,7 @@ $('#yes').click(function() {
             Swal.fire({
                 width: 900,
                 confirmButtonText: CONFIG.btnAccept,
-                background: '#fff url("img/iput-KakaoTalk_20240307_000806300_01.jpg")',
+                background: '#fff url("img/iput-KakaoTalk_20240307_000806300.jpg")',
                 title: CONFIG.mess,
                 text: CONFIG.messDesc,
                 confirmButtonColor: '#83d0c9',
